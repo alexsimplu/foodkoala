da19149b853b2157a1cf0e69b47546d3ba84a45f
Csaszar Sandor <csaszar@mebedo.eu>
2020-10-07 00:03:52 +0300

    added firebase to the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import * as firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyAFBV1vWOBT3WyWrfaTtvm4Y8DqYVxLcVk',
+    authDomain: 'foof-koala.firebaseapp.com',
+    databaseURL: 'https://foof-koala.firebaseio.com',
+    projectId: 'foof-koala',
+    storageBucket: 'foof-koala.appspot.com',
+    messagingSenderId: '758863475140',
+    appId: '1:758863475140:web:d6cefd8bd5b82f7acff491',
+    measurementId: 'G-TTHCDDCHYM',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'>Food Koala</header>
+        </div>
+    );
 }
 
 export default App;
